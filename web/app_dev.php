b9ab3018ec0c6a674181d248dcad3753aec1b79e
c33s <dev@c33s.net>
2013-04-18 14:43:37 +0200

    added ip restriction to app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,4 +1,10 @@
 <?php
+$admin_ips = require __DIR__.'/../config/ip.php'; 
+if (! in_array($_SERVER['REMOTE_ADDR'],$admin_ips)) 
+{
+    header("HTTP/1.0 403 Forbidden");
+    exit('Forbidden');
+}
 
 use Symfony\Component\ClassLoader\DebugClassLoader;
 use Symfony\Component\HttpKernel\Debug\ErrorHandler;
